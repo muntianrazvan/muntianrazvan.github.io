ef658f31056ba8cb8af898be77d8b9bf33fc1657
Razvan Muntian <catalinm.work@gmail.com>
2017-08-10 00:40:55 +0300

    resume update

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+$(".arrow").click(function(e) {
+    e.preventDefault();
+    var href = $(this).attr('href');
+    $('html, body').animate({
+        scrollTop: $(href).offset().top
+    }, 500);
+});
